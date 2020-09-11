55636885749f3ff44d206c2cfc42c29801dc843e
pablo malczewski <malczewskipablo@gmail.com>
2020-09-11 13:49:48 -0300

    Add favicon

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,13 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("channels")
+require("jquery")
+require("semantic-ui-sass")
 
+$(document).on('turbolinks:load', function() {
+    $('.ui.dropdown').dropdown();
+
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
